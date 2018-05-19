06e0cc4be556af3c8ce0a5425f95e105e5a3b35b
Ephrame <viollet.ame@gmail.com>
2018-05-19 13:17:52 +0200

    [UI] add little composant

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, AsyncStorage } from 'react-native';
 import MainTabView from './components/MainTabView';
 import { Alert } from 'react-native'
-import { Button } from 'antd-mobile';
+import { Button } from 'antd-mobile/lib';
 import { CardDashBoard } from './views/IssusDashboard';
 
 
@@ -24,22 +24,23 @@ export default class App extends React.Component {
     })
       .then(response => response.json())
       .then(responseData => {
-        this.setState({ issues: responseData });
-        Alert.alert(
-          "Success",
-          "Backend call to the Github API was successful",
-          [
-            {
-              text: "Cancel",
-              onPress: () => console.log("Cancel Pressed"),
-              style: "cancel"
-            },
-            { text: "OK", onPress: () => console.log("OK Pressed") }
-          ],
-          { cancelable: false }
-        );
-        console.log("Fetch from Github API was successful!");
-        console.log(self.state.issues)
+        return AsyncStorage.setItem('issues', JSON.stringify(responseData)).then(v => {
+          this.setState({ issues: responseData });
+          Alert.alert(
+            "Success",
+            "Backend call to the Github API was successful",
+            [
+              {
+                text: "Cancel",
+                onPress: () => console.log("Cancel Pressed"),
+                style: "cancel"
+              },
+              { text: "OK", onPress: () => console.log("OK Pressed") }
+            ],
+            { cancelable: false }
+          );
+          console.log("Fetch from Github API was successful!");
+        })
       })
       .catch(error => {
         console.log("Fetch from Github API failed:", error);
--- views/IssusDashboard.js
+++ views/IssusDashboard.js
@@ -1,20 +1,49 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
-import { Card } from 'antd-mobile'
+import { StyleSheet, View, Text, AsyncStorage } from 'react-native'
+import { Card } from 'antd-mobile/lib'
 
-export class CardDashBoard extends React.Component {
+
+class SimpleCard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      issues: []
+    };
+  }
+
+  componentDidMount() {
+
+  }
+
   render () {
+    const { title, content } = this.props
     return (
       <View style={styles.container}>
         <Card full>
           <Card.Header
-            title='Travis Issues'
+            title={title}
             thumb='https://user-images.githubusercontent.com/10349407/40267068-b83cdfda-5b56-11e8-86ab-7a9af06224db.png'
           />
           <Card.Body>
-            <Text>Amazing</Text>
+            <Text>{content}</Text>
           </Card.Body>
         </Card>
+      </View>
+    )
+  }
+}
+
+export class CardDashBoard extends React.Component {
+  render () {
+    console.log(JSON.stringify(this.props.issues, 0, 3))
+    return (
+      <View style={styles.container}>
+        <Text> Les issues </Text>
+        {
+          this.props.issues.map(elm => {
+            return <SimpleCard key={elm.id} title={elm.name} />
+          })
+        }
       </View>
     )
   }
