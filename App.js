fce9c27a6f7ef0b7fa02c2c10c9f2624a1d28a18
Timo Stollenwerk <tisto@plone.org>
2018-05-19 11:45:28 +0200

    Add simple example of how to do a backend call to the github api.

--- App.js
+++ App.js
@@ -1,8 +1,48 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MainTabView from './components/MainTabView';
+import { Alert } from 'react-native'
+
 
 export default class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      issues: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://api.github.com/users/tisto/repos", {
+      headers: {
+        Accept: "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(responseData => {
+        this.setState({ issues: responseData });
+        Alert.alert(
+          "Success",
+          "Backend call to the Github API was successful",
+          [
+            {
+              text: "Cancel",
+              onPress: () => console.log("Cancel Pressed"),
+              style: "cancel"
+            },
+            { text: "OK", onPress: () => console.log("OK Pressed") }
+          ],
+          { cancelable: false }
+        );
+        console.log("Fetch from Github API was successful!");
+        console.log(self.state.issues)
+      })
+      .catch(error => {
+        console.log("Fetch from Github API failed:", error);
+      });
+  }
+
   render() {
     return <MainTabView />;
   }
